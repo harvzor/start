07470332ff7181dfe7c9f7bc21e1fe3daac86ec0
Harvey Williams <harveyjwilliams@aol.co.uk>
2016-03-19 20:22:11 +0000

    Improved scraper to get more information about the image and store it in JSON.

--- server/backgrounds.js
+++ server/backgrounds.js
@@ -10,12 +10,37 @@ var backgrounds = function(dependencies) {
 	var publicBackgroundsPath = '/media/backgrounds/';
 	var backgroundsPath = 'public' + publicBackgroundsPath;
 
+	var backgroundData = function() { 
+		var data = {
+			url: '',
+			date: '',
+			name: '',
+			credit: '',
+			description: ''
+		};
+
+		return {
+			set: function(key, value) {
+				if (typeof data[key] != 'undefined') {
+					data[key] = value;
+				}
+			},
+			get: function(key) {
+				return data[key];
+			},
+			getAll: function() {
+				return data;
+			}
+		};
+	};
+
 	// Scrapes National Geographic for their background today.
-	var scrapeForImageUrl = function(callback) {
+	var scrapeForImageData = function(callback) {
 		request({
 			method: 'GET',
 			url: 'http://photography.nationalgeographic.com/photography/photo-of-the-day/'
 		}, function(err, response, html) {
+			var data = new backgroundData();
 			var $;
 
 			if (err) {
@@ -26,15 +51,47 @@ var backgrounds = function(dependencies) {
 
 			$ = cheerio.load(html);
 
-			$('.primary_photo img').filter(function() {
-				callback('http:' + $(this).attr('src'));
-			});
+			data.set('url', 'http:' + $('.primary_photo img').attr('src'));
+			data.set('date', helpers.dateToString(new Date()));
+			data.set('name', $('h1').text());
+			data.set('credit', $('#caption .credit').text());
+			data.set('description', $('#caption .credit').next().text());
+
+			callback(data);
 		});
 	};
 
 	var saveImage = function(url, filePath, callback) {
 		request(url).pipe(fs.createWriteStream(filePath)).on('close', callback);
 	};
+
+	var jsonFileDataConstructor = function() {
+		var dataPath = 'data/backgrounds.json';
+		var data = null;
+
+		return {
+			get: function(loadNew) {
+				if (loadNew || data == null) {
+					return JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+				}
+
+				return data;
+			},
+			set: function(newData) {
+				var data = this.get();
+
+				data.push(newData);
+
+				fs.writeFile(dataPath, JSON.stringify(data), function(err) {
+					if (err) {
+						logger.info('[backgrounds] Error writing to backgrounds.json');
+					}
+				});
+			}
+		};
+	};
+	
+	var jsonFileData = new jsonFileDataConstructor();
 
 	return {
 		// Gets a background from the file system.
@@ -53,11 +110,12 @@ var backgrounds = function(dependencies) {
 			}
 			// If the file requested is for todays image of the day
 			else if (dateString == helpers.dateToString(new Date())) {
-
-				scrapeForImageUrl(function(url) {
-					saveImage(url, backgroundsPath + dateString + '.jpg', function() {
+				scrapeForImageData(function(data) {
+					saveImage(data.get('url'), backgroundsPath + dateString + '.jpg', function() {
 						callback(publicBackgroundsPath + dateString + '.jpg');
 					});
+
+					jsonFileData.set(data.getAll());
 				});
 			} else {
 				logger.info('[backgrounds]', 'No background found for date: ' + dateString);
